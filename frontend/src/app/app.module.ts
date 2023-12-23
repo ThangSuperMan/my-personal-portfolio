cb04efcc7946b8ab759362a1c7381de01331dea5
Phan Tan Thang <thangphan@192.168.2.212>
2023-12-23 16:18:47 +0700

    feat: add project structure and header component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,11 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
+import { PostComponent } from './posts/posts.component';
+import { HeaderComponent } from './shared/layout/header/header.component';
+import { FooterComponent } from './shared/layout/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { BioComponent } from './home/bio/bio.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    PostComponent,
+    ProjectsComponent,
+    HeaderComponent,
+    BioComponent,
+    FooterComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ frontend/src/app/core/models/post.ts
@@ -0,0 +1,7 @@
+export default interface Post {
+  title: string;
+  body: string;
+  image_url: string;
+  created_at: string;
+}
+
