d9f13eadc25a287e32bf1929926e584df9cb4bf2
Phan Tan Thang <thangphan@192.168.2.212>
2023-12-24 22:02:52 +0700

    feat: support create multiple psql databases and users

---
+++ server/scripts/init-multiple-psql-databases.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -eu
+
+function create_user_and_datebase() {
+  local database=$1
+  local user=$1
+  echo "Create user and database: $database"
+  
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    SELECT current_user;
+    CREATE DATABASE $database;
+    CREATE USER $user with password '$POSTGRES_NEW_DB_PASSWORD';
+    GRANT ALL PRIVILEGES ON DATABASE $database TO $user;
+EOSQL
+
+  echo "Grant privilege schema public for user: $database"
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$database" <<-EOSQL
+    SELECT current_user;
+    GRANT CREATE ON SCHEMA public TO $user;
+EOSQL
+}
+
+if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
+  echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
+  for db in $(echo "$POSTGRES_MULTIPLE_DATABASES" | tr  ',' ' '); do
+    create_user_and_datebase "$db"
+  done
+  echo "Mutiple databases created"
+fi
