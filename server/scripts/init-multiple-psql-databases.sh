58c2b334f3bfce4111b5c96b3d3157a83be6d381
Phan Tan Thang <thangphan@192.168.2.212>
2023-12-26 12:45:41 +0700

    fix: remove unnecessary queries

--- server/scripts/init-multiple-psql-databases.sh
+++ server/scripts/init-multiple-psql-databases.sh
@@ -8,7 +8,6 @@ function create_user_and_datebase() {
   echo "Create user and database: $database"
   
   psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    SELECT current_user;
     CREATE DATABASE $database;
     CREATE USER $user with password '$POSTGRES_NEW_DB_PASSWORD';
     GRANT ALL PRIVILEGES ON DATABASE $database TO $user;
@@ -16,7 +15,6 @@ EOSQL
 
   echo "Grant privilege schema public for user: $user"
   psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$database" <<-EOSQL
-    SELECT current_user;
     GRANT CREATE ON SCHEMA public TO $user;
 EOSQL
 }
